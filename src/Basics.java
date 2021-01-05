e70152af0123363cf37e7719b401b9afe93ad24a
infantaprishika <infantaprishika@gmail.com>
2021-01-05 13:47:30 +0530

    first commit

---
+++ src/Basics.java
@@ -0,0 +1,66 @@
+
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+import org.testng.Assert;
+
+import files.ReUsableMethods;
+import files.payload;
+
+public class Basics {
+
+	public static void main(String[] args) {
+		
+		
+		//given - all input details 
+		//when - Submit the API -resource,http method
+		//Then - validate the response
+		
+		RestAssured.baseURI= "petclinicapi.e46708b92c054086909b.eastus.aksapp.io/petclinic";
+		String response=given().log().all().header("Content-Type","application/json")
+		.body(payload.AddVet()).when().post("api/vets")
+		.then().assertThat().statusCode(200)
+		.extract().response().asString();
+		
+		System.out.println(response);
+		
+		JsonPath js=new JsonPath(response); //for parsing Json
+		String vetId=js.getString("vet_id");
+		
+		System.out.println(vetId);
+		
+		
+		String name = "psychology";
+		//Update Vet
+		
+		given().log().all().header("Content-Type","application/json")
+		.body("\"specialties\": [\r\n" + 
+				"    {\r\n" + 
+				"      \"id\":\""+vetId+"\",\r\n" + 
+				"      \"name\":\""+name+"\",\r\n" + 
+				"    }\r\n" + 
+				"  ]").
+		when().put("api/vets")
+		.then().assertThat().log().all().statusCode(200).body("msg", equalTo("Address successfully updated"));
+		
+		
+		
+		//Get Vet
+		
+	String getVetResponse=	given().log().all()
+		.when().get("/api/vets")
+		.then().assertThat().log().all().statusCode(200).extract().response().asString();
+	
+	JsonPath js1=ReUsableMethods.rawToJson(getVetResponse);
+	String firstName =js1.getString("Name");
+	System.out.println(firstName);
+	Assert.assertEquals(firstName, "prishika");
+	//Cucumber Junit, Testng
+		
+		
+	}
+
+}
---
+++ src/files/payload.java
@@ -0,0 +1,27 @@
+package files;
+
+public class payload {
+
+	
+	public static String AddVet()
+	
+	{
+		return "{\r\n" + 
+				"  \"firstName\": \"prishika\",\r\n" + 
+				"  \"id\": 313,\r\n" + 
+				"  \"lastName\": \"peter\",\r\n" + 
+				"  \"specialties\": [\r\n" + 
+				"    {\r\n" + 
+				"      \"id\": 1,\r\n" + 
+				"      \"name\": \"abc\"\r\n" + 
+				"    }\r\n" + 
+				"  ]\r\n" + 
+				"}";
+		
+		
+	}
+	
+
+	
+	
+}
